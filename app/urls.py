a56a9b364367eb549809371d12e6f1f308969940
konekato <18ec033@ms.dendai.ac.jp>
2020-05-01 19:45:53 +0900

    add: connect /uploads/ and /uploads/<>/

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'app'
+
 urlpatterns = [
     path('', views.upload_details, name='upload_details'),
     path('<int:detail_id>/', views.upload_file, name='upload_file')
