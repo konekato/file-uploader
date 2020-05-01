340d1ddea8f4c9c845e09b891012461f103e5fc4
konekato <18ec033@ms.dendai.ac.jp>
2020-05-01 19:32:15 +0900

    fix: urlパターン修正

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
-    path('uploads/', views.upload_details, name='upload_details')
+    path('', views.upload_details, name='upload_details'),
 ]
--- file_uploader/urls.py
+++ file_uploader/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
+from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('django.contrib.auth.urls')),
-    path('', include('app.urls'))
+    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
+    path('uploads/', include('app.urls'))
 ]
