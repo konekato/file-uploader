dac6b2eb9f04664f5403d7d70959558b87769c3e
konekato <18ec033@ms.dendai.ac.jp>
2020-05-01 18:17:25 +0900

    add: models and migration

--- app/models.py
+++ app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class UploadDetail(models.Model):
+    title = models.CharField(max_length=50)
+    text = models.TextField(null=True)
+    created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+class UploadFile(models.Model):
+    file = models.FileField()
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    detail = models.ForeignKey(UploadDetail, on_delete=models.CASCADE)
+    created = models.DateTimeField(default=timezone.now)
