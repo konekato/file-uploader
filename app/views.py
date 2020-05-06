f651b9662d58360aba5a2fb3ee0a6325cd2bb851
konekato <18ec033@ms.dendai.ac.jp>
2020-05-06 13:16:21 +0900

    add: viewに実装

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import UploadDetail, UploadFile
-
+from .forms import UploadFileForm
 
 def home(request):
     return render(request, 'home.html')
@@ -13,6 +13,8 @@ def upload_details(request):
 
 def upload_file(request, detail_id):
     detail = UploadDetail.objects.get(id=detail_id)
+    form = UploadFileForm
     return render(request, 'upload_file.html', context={
         'detail': detail,
+        'form': form,
     })
